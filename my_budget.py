d199edbf317108b79665f4f4ef1696f9f23a67e6
Łukasz Franczyk <lukasz.m.franczyk@gmail.com>
2026-03-24 13:27:19 +0100

    Revert "Add function create_app()"
    This reverts commit 06642d5bb3b809c8062b1a90626161ee1fe4101b.
    
    # Conflicts:
    #	my_budget.py

--- my_budget.py
+++ my_budget.py
@@ -1,10 +1,10 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import create_app, db
-from app.models import User, Income, Expense
+from app.models import User, Income
 
 app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Income': Income, 'Expense': Expense}
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Income': Income}
